65e5948b0214b3a6d11a9ee993751eb3d0e8a327
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-07 11:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let btn = document.getElementsByTagName("input")[0];
+
+let selectElements = document.getElementById("colorSelect");
+
+btn.addEventListener("click", () => {
+	for(let i = 0; i < selectElements.options.length; i++){
+	if(selectElements.options[i].selected) selectElements.remove(i);
+}
+});
+
